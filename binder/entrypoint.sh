4d70947e5358a8887d88a8890bb14c071b106f68
Yanxiang Zhan <yanxiang@uni-bremen.de>
2025-07-02 15:59:57 +0200

    proxy config in dockerfile

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,31 +5,4 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 roslaunch --wait pycram ik_and_description.launch &
 
-# Config Vscode in launcher
-if [[ -n "$JUPYTERHUB_CLIENT_ID" ]]; then
-    VS_BASE_PATH="/user/$JUPYTERHUB_CLIENT_ID"
-else
-    VS_BASE_PATH=""
-fi
-echo "
-c.ServerProxy.servers = {
-    'code': {
-        'command': ['code-server',
-                    '--auth=none', 
-                    '--abs-proxy-base-path=$VS_BASE_PATH/code',
-                    '--disable-workspace-trust',
-                    ],
-        'timeout': 20,
-        'port': 8080,
-        'launch_url': '/code/?folder=$PWD',
-        'launcher_entry': {
-            'title': 'Visual Studio Code',
-            'icon_path': '$HOME/vscode.svg',
-        },
-        'new_browser_tab': True
-    }
-}
-" >> ~/.jupyter/jupyter_notebook_config.py
-
-
 exec "$@"
