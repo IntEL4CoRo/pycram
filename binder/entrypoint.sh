62b6d41d8c13624e2eac4e24737b97823037e4e1
Yanxiang Zhan <yanxiang@uni-bremen.de>
2025-07-02 10:14:28 +0200

    update proxy path

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,6 +5,6 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 roslaunch --wait pycram ik_and_description.launch &
 
-code-server --auth=none --abs-proxy-base-path=/proxy/8080/ &
-
+# code-server --auth=none --abs-proxy-base-path=/proxy/8080/ &
+code-server --auth=none --abs-proxy-base-path=/user/$JUPYTERHUB_CLIENT_ID/proxy/8080 &
 exec "$@"
