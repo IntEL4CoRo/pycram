c1c256dd76191fccb136fb8786dbfa12169431a1
Yanxiang Zhan <yanxiang@uni-bremen.de>
2025-07-02 13:10:55 +0200

    add launcher

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,6 +5,31 @@ roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 roslaunch --wait pycram ik_and_description.launch &
 
-# code-server --auth=none --abs-proxy-base-path=/proxy/8080/ &
-code-server --auth=none --abs-proxy-base-path=/user/$JUPYTERHUB_CLIENT_ID/proxy/8080 &
+# Config Vscode in launcher
+if [[ -n "$JUPYTERHUB_CLIENT_ID" ]]; then
+    VS_BASE_PATH="/user/$JUPYTERHUB_CLIENT_ID"
+else
+    VS_BASE_PATH=""
+fi
+echo "
+c.ServerProxy.servers = {
+    'code': {
+        'command': ['code-server',
+                    '--auth=none', 
+                    '--abs-proxy-base-path=$VS_BASE_PATH/code',
+                    '--disable-workspace-trust',
+                    ],
+        'timeout': 20,
+        'port': 8080,
+        'launch_url': '/code/?folder=/home',
+        'launcher_entry': {
+            'title': 'Visual Studio Code',
+            'icon_path': '$HOME/vscode.svg',
+        },
+        'new_browser_tab': True
+    }
+}
+" >> ~/.jupyter/jupyter_notebook_config.py
+
+
 exec "$@"
