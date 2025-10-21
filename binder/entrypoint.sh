b2bd3f64b93c426717f310b3d5f3dea34dbe5db6
Yanxiang Zhan <yanxiang@uni-bremen.de>
2025-10-21 10:44:35 +0200

    update to ROS2

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source ${PYCRAM_WS}/devel/setup.bash
-roscore &
-roslaunch --wait rvizweb rvizweb.launch &
-roslaunch --wait pycram ik_and_description.launch &
+source ${ROS_PATH}/setup.bash
+source ${ROS_WS}/install/setup.bash
 
 exec "$@"
